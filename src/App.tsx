52ba45c2148a5bccfb6b508ca02fdcf5012b2649
brocchirodrigo <rodrigo.brocchi@gmail.com>
2020-11-23 20:53:17 -0300

    Conclusão página de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
+
+import SignIn from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
-    <h1>Helo World</h1>
+    <SignIn />
+    <GlobalStyle />
   </>
 );
 
