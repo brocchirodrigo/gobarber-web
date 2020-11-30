6789180cd49e2b8ef5050231bbae49358d803cea
brocchirodrigo <rodrigo.brocchi@gmail.com>
2020-11-30 20:26:15 -0300

    Autenticação de rotas

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  const h1 = 'DashBoard';
+
+  return (
+    <>
+      <h1>{h1}</h1>
+    </>
+  );
+};
+
+export default Dashboard;
