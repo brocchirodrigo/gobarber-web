119c917ba5d1eba1d7379c18a5a793b4e4063783
brocchirodrigo <rodrigo.brocchi@gmail.com>
2020-12-06 01:53:38 -0300

    Opção para deslogar web

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import Button from '../../components/Button';
+import { useAuth } from '../../hooks/Auth';
 
 const Dashboard: React.FC = () => {
+  const { signOut } = useAuth();
   const h1 = 'DashBoard';
+  const test = 'Página privada';
 
   return (
     <>
       <h1>{h1}</h1>
+      <p>{test}</p>
+      <Button style={{ width: '20%' }} type="button" onClick={signOut}>
+        Deslogar
+      </Button>
     </>
   );
 };
